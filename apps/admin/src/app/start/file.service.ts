0fe7c73d2cd95ee260fab00c6fb3ab1c59a6f3d4
mikepziegler <info@mikepziegler.com>
2025-06-20 10:57:15 +0200

    Adding service to frontend

--- apps/admin/src/app/start/file.service.ts
+++ apps/admin/src/app/start/file.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 
 @Injectable()
 export class FileService {
-  constructor(private http: HttpClient) { }
+
+  private http: HttpClient = inject(HttpClient)
+
+  getAllFiles(): Observable<any> {
+
+    return this.http.get(environment.apiUrl + '/files');
+
+  }
+
+  uploadFile(files: FileList): Observable<{ path: string }> {
+    const formData = new FormData();
+    for (const file of Array.from(files)) {
+      formData.append('file', file); // Change to `files[]` if backend expects an array
+    }
+
+    return this.http.post<{ path: string }>(`${environment.apiUrl}/files/upload`, formData);
+  }
 }
--- apps/admin/src/app/start/start.component.ts
+++ apps/admin/src/app/start/start.component.ts
@@ -1,32 +1,31 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { Component, inject, OnInit } from '@angular/core';
+import { FileService } from './file.service';
 
 @Component({
   selector: 'app-start',
   template: `
-    <input
-      type="file"
-      (change)="onFileSelected($event)"
-      multiple
-      accept="*"
-    />
-  `
+    <input type="file" (change)="onFileSelected($event)" multiple accept="*" />
+  `,
+  providers: [FileService],
 })
-export class StartComponent {
-  constructor(private http: HttpClient) {}
+export class StartComponent implements OnInit {
+  private fileService = inject(FileService);
+
+  ngOnInit() {
+
+    this.fileService.getAllFiles().subscribe(files => {
+
+      console.log(files);
+    })
+
+  }
 
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
 
     if (!input.files?.length) return;
 
-    const formData = new FormData();
-    for (const file of Array.from(input.files)) {
-      formData.append('file', file); // Change to `files[]` if backend expects an array
-    }
-
-    this.http.post<{ path: string }>(`${environment.apiUrl}/files/upload`, formData).subscribe({
+    this.fileService.uploadFile(input.files).subscribe({
       next: (res: any) => console.log('Upload successful:', res),
       error: (err: any) => console.error('Upload failed:', err),
     });
